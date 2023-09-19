cc5e10a19c3aa2c5c312c87c7c60073183d90e00
ANA MARTINEZ <145292689+Anamar38@users.noreply.github.com>
2023-09-19 03:29:48 +0000

    guardar

---
+++ Ejercicio4.py
@@ -0,0 +1,6 @@
+# Solicitar al usuario el ingreso del número
+ingreso_numero = int(input("Ingrese un número entero positivo: "))
+# Calculo de la suma
+suma_numeros = ingreso_numero*(ingreso_numero+1)/2
+# Suma de numeros
+print(f"La suma de los primeros {ingreso_numero} enteros positivos es: {suma_numeros}")
